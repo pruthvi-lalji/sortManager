ec291b2d7edbac1a4918c5cf0a6d5016de9241e5
Pruthvi Lalji <pruthvi_lalji@hotmail.co.uk>
2021-12-06 21:01:08 +0000

    Test cases fixed, added Binary Tree sort test cases

--- src/test/java/com/spartaglobal/sort/test/BinaryTreeSortTest.java
+++ src/test/java/com/spartaglobal/sort/test/BinaryTreeSortTest.java
@@ -1,64 +1,57 @@
-//package com.spartaglobal.sort.test;
-//
-//import org.junit.jupiter.api.DisplayName;
-//import org.junit.jupiter.api.Test;
-//
-//import java.util.ArrayList;
-//import java.util.List;
-//
-//public class BinaryTreeSortTest {
-//
-//    BST bst = new BST();
-//
-//
-//    @DisplayName("Empty array")
-//    @Test
-//    public void emptyArrayTest(){
-//        Integer[] array = {};
-//        bst.run(array);
-//        assertEquals(null, bst.run(array));
-//
-//    }
-//
-//    @DisplayName("One element array")
-//    @Test
-//    public void oneElementArrayTest(){
-//        Integer[] array = {1};
-//        assertArrayEquals(array, bst.run(array));
-//    }
-//
-//    @DisplayName("Array sorter")
-//    @Test
-//    public void arrayTest(){
-//        Integer[] array = {1,2,9,7,3,8,4,5,6,0,1};
-//        Integer[] sorted = {0,1,2,3,4,5,6,7,8,9};
-//        assertArrayEquals(sorted, bst.run(array));
-//    }
-//
-//
-//
-//    @DisplayName("Empty ArrayList")
-//    @Test
-//    public void emptyArrayListTest(){
-//        List<Integer> arrayList= new ArrayList<>();
-//        assertEquals(null, bst.run(arrayList));
-//    }
-//
-//
-//    @DisplayName("Arraylist with one element")
-//    @Test
-//    public void oneElementListTest(){
-//        List<Integer> arrayList = new ArrayList<>(List.of(5));
-//        assertEquals("[5]", bst.run(arrayList).toString());
-//    }
-//
-//
-//    @DisplayName("Arraylist Test")
-//    @Test
-//    public void ArrayListTest(){
-//        List<Integer> arrayList = new ArrayList<>(List.of(1,2,9,7,3,8,4,5,6,0,1));
-//        List<Integer> unsortedList = new ArrayList<>(List.of(0,1,2,3,4,5,6,7,8,9));
-//        assertEquals(unsortedList.toString(), bst.run(arrayList).toString());
-//    }
-//
-//}
+package com.spartaglobal.sort.test;
+
+import com.spartaglobal.sort.model.TreeSort;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class BinaryTreeSortTest {
+    TreeSort treeSort = new TreeSort();
+
+    @DisplayName("Empty array")
+    @Test
+    public void emptyArrayTest(){
+        Integer[] array = {};
+        assertEquals(null, treeSort.sort(array));
+    }
+    @DisplayName("One element array")
+    @Test
+    public void oneElementArrayTest(){
+        Integer[] array = {1};
+        assertEquals(Arrays.asList(array), treeSort.sort(array));
+    }
+
+    @DisplayName("Array sorter")
+    @Test
+    public void arrayTest(){
+        Integer[] array = {1,2,9,7,3,8,4,5,6,0,1};
+        Integer[] sorted = {0,1,2,3,4,5,6,7,8,9};
+        assertEquals(Arrays.asList(sorted), treeSort.sort(array));
+    }
+
+    @DisplayName("Empty ArrayList")
+    @Test
+    public void emptyArrayListTest(){
+        List<Integer> arrayList= new ArrayList<>();
+        assertEquals(null, treeSort.sort(arrayList));
+    }
+
+    @DisplayName("Arraylist with one element")
+    @Test
+    public void oneElementListTest(){
+        List<Integer> arrayList = new ArrayList<>(List.of(5));
+        assertEquals(5, treeSort.sort(arrayList).get(0));
+    }
+        @DisplayName("Arraylist Test")
+    @Test
+    public void ArrayListTest(){
+        List<Integer> arrayList = new ArrayList<>(List.of(1,2,9,7,3,8,4,5,6,0,1));
+        List<Integer> unsortedList = new ArrayList<>(List.of(0,1,2,3,4,5,6,7,8,9));
+        assertEquals(unsortedList, treeSort.sort(arrayList));
+    }
+}
--- src/test/java/com/spartaglobal/sort/test/GenericQuickSortTest.java
+++ src/test/java/com/spartaglobal/sort/test/GenericQuickSortTest.java
@@ -1,145 +1,92 @@
-//package com.spartaglobal.sort.test;
-//
-//import org.junit.jupiter.api.Disabled;
-//import org.junit.jupiter.api.DisplayName;
-//import org.junit.jupiter.api.Test;
-//
-//import java.util.*;
-//
-//import static org.junit.jupiter.api.Assertions.*;
-//
-//@Disabled
-//public class GenericQuickSortTest {
-//
-//    public static GenericSorter qs = new GenericQuickSort();
-//    @DisplayName("ArrayList Test")
-//    @Test
-//    public void arrayListTest(){
-//        List<Integer> arrayList = new ArrayList<>(List.of(-5,8,-7,45,-8,-78,3,4,-6,71));
-//        List<Integer> expectedResult = new ArrayList<>();
-//        expectedResult.addAll(arrayList);
-//        List<Integer> actualResult = qs.sortList(arrayList);
-//        Collections.sort(expectedResult);
-//        assertArrayEquals(expectedResult.toArray(), actualResult.toArray());
-//    }
-//
-//    @DisplayName("Empty ArrayList ")
-//    @Test
-//    public void emptyNullArrayList(){
-//        List<Integer> arrayList = new ArrayList<>();
-//        List<Integer> actualResult = qs.sortList(arrayList);
-//        assertNotNull(actualResult);
-//    }
-//
-//    @DisplayName("Null ArrayList ")
-//    @Test
-//    @Disabled
-//    public void nullArrayList(){
-//        List<Integer> arrayList = null;
-//        List<Integer> actualResult = qs.sortList(arrayList);
-//        assertEquals(null, actualResult);
-//    }
-//
-//    @DisplayName("Generic Test Random ")
-//    @Test
-//    public void randomNumberGeneratedList(){
-//        RandomArrayGenerator2 rd = new RandomArrayGenerator2(Integer.class);
-//        List<Integer> arrayList;
-//        arrayList = rd.randArrayList(1000);
-//        List<Integer> expectedResult = new ArrayList<>();
-//        expectedResult.addAll(arrayList);
-//        List<Integer> actualResult = qs.sortList(arrayList);
-//        Collections.sort(expectedResult);
-//        assertArrayEquals(expectedResult.toArray(), actualResult.toArray());
-//    }
-//
-//
-//    @DisplayName("One Element List")
-//    @Test
-//    public void oneElementList(){
-//        List<Integer> arrayList = new ArrayList<>();
-//        arrayList.add(6);
-//        List<Integer> actualResult = qs.sortList(arrayList);
-//        assertEquals(arrayList,actualResult);
-//    }
-//
-//
-//    @DisplayName("ArrayList Double")
-//    @Test
-//    public void arrayListWithDoubles(){
-//        List<Double> arrayList;
-//        RandomArrayGenerator2 rd = new RandomArrayGenerator2(Double.class);
-//        arrayList = rd.randArrayList(20);
-//        List<Double> expectedResult = new ArrayList<>();
-//        expectedResult.addAll(arrayList);
-//        Collections.sort(expectedResult);
-//        List<Double> actualResult = qs.sortList(arrayList);
-//        assertEquals(expectedResult, actualResult);
-//    }
-//
-//
-//
-//
-//
-//
-//
-//    @DisplayName("Array Test")
-//    @Test
-//    public void arrayTest(){
-//        Integer[] array = {1,8,4,9,2,7,2,8,4,8,5,6,7,3,9,8,1,8,4,27,75};
-//        Integer[] expectedResult = Arrays.copyOf(array,array.length);
-//        Integer[] actualResult = qs.sortArray(array);
-//        Arrays.sort(expectedResult);
-//        assertArrayEquals(expectedResult, actualResult);
-//    }
-//
-//
-//    @DisplayName("Null Array")
-//    @Test
-//    public void nullArray(){
-//        Integer[] array = null;
-//        assertEquals(null, qs.sortArray(array));
-//    }
-//
-//    @DisplayName("Empty Array")
-//    @Test
-//    public void emptyArray(){
-//        Integer[] array = {};
-//        assertEquals(null, qs.sortArray(array));
-//    }
-//
-//
-//    @DisplayName("Random Array with negative Numbers")
-//    @Test
-//    public void randomGeneratedArray(){
-//        RandomArrayGenerator2 rd = new RandomArrayGenerator2(Integer.class);
-//        Integer[] array = (Integer[]) rd.randArray(100);
-//        Integer[] expectedResult = Arrays.copyOf(array, array.length);
-//        Integer[] actualResult = qs.sortArray(array);
-//        Arrays.sort(expectedResult);
-//        assertArrayEquals(expectedResult,actualResult);
-//     }
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//}
+package com.spartaglobal.sort.test;
+
+import com.spartaglobal.sort.controller.GenericSorter;
+import com.spartaglobal.sort.model.GenericQuickSort;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@Disabled
+public class GenericQuickSortTest {
+    GenericSorter quickSort = new GenericQuickSort();
+
+    @DisplayName("ArrayList Test")
+    @Test
+    public void arrayListTest() {
+        List<Integer> arrayList = new ArrayList<>(List.of(-5, 8, -7, 45, -8, -78, 3, 4, -6, 71));
+        List<Integer> expectedResult = new ArrayList<>();
+        expectedResult.addAll(arrayList);
+        List<Integer> actualResult = quickSort.sort(arrayList);
+        Collections.sort(expectedResult);
+        assertArrayEquals(expectedResult.toArray(), actualResult.toArray());
+    }
+
+    @DisplayName("Empty ArrayList ")
+    @Test
+    public void emptyNullArrayList() {
+        List<Integer> arrayList = new ArrayList<>();
+        assertEquals(null, quickSort.sort(arrayList));
+    }
+
+
+    @DisplayName("Null ArrayList ")
+    @Test
+    @Disabled
+    public void nullArrayList() {
+        List<Integer> arrayList = null;
+        List<Integer> actualResult = quickSort.sort(arrayList);
+        assertEquals(null, actualResult);
+    }
+
+
+    @DisplayName("One Element List")
+    @Test
+    public void oneElementList() {
+        List<Integer> arrayList = new ArrayList<>();
+        arrayList.add(6);
+        List<Integer> actualResult = quickSort.sort(arrayList);
+        assertEquals(arrayList, actualResult);
+    }
+
+
+    @DisplayName("ArrayList Double")
+    @Test
+    public void arrayListWithDoubles() {
+        List<Double> arrayList = new ArrayList(List.of(3.5,6.4,8.4,5.0,4.1,238.4,12.2,3.5,8.9,2.1));
+        List<Double> expectedResult = new ArrayList<>(arrayList);
+        Collections.sort(expectedResult);
+        assertEquals(expectedResult, quickSort.sort(arrayList));
+    }
+
+
+    @DisplayName("Array Test")
+    @Test
+    public void arrayTest() {
+        Integer[] array = {1, 8, 4, 9, 2, 7, 2, 8, 4, 8, 5, 6, 7, 3, 9, 8, 1, 8, 4, 27, 75};
+        Integer[] expectedResult = Arrays.copyOf(array, array.length);
+        Arrays.sort(expectedResult);
+        assertArrayEquals(expectedResult, quickSort.sortArray(array));
+    }
+
+
+    @DisplayName("Null Array")
+    @Test
+    public void nullArray() {
+        Integer[] array = null;
+        assertEquals(null, quickSort.sortArray(array));
+    }
+
+    @DisplayName("Empty Array")
+    @Test
+    public void emptyArray() {
+        Integer[] array = {};
+        assertEquals(null, quickSort.sortArray(array));
+    }
+
+
+}
+
